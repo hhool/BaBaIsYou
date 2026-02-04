5882bbeac7c9cd401e810e6aefa41d50af97db49
hhool <hhool@outlook.com>
2026-02-05 01:48:19 +0800

    feat(home): add info toggle button; hide wiki/github links

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -15,14 +15,14 @@
           Start
         </button>
       </router-link>
-      <a href='https://babaiswiki.fandom.com/wiki/Baba_Is_You_Wiki' rel='noreferrer noopener' target='_blank'>
+      <a class='hidden' href='https://babaiswiki.fandom.com/wiki/Baba_Is_You_Wiki' rel='noreferrer noopener' target='_blank'>
         <button
           class='my-10 py-1 px-2 sm:(py-2 px-4) flex justify-center items-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg '
           type='button'>
           Wiki
         </button>
       </a>
-      <a href='https://github.com/Xanonymous-GitHub/BaBaIsYou' rel='noreferrer noopener' target='_blank'>
+      <a class='hidden' href='https://github.com/Xanonymous-GitHub/BaBaIsYou' rel='noreferrer noopener' target='_blank'>
         <button
           class='my-10 py-1 px-2 sm:(py-2 px-4) flex justify-center items-center bg-gray-900 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg '
           type='button'>
@@ -30,27 +30,49 @@
         </button>
       </a>
     </div>
-    <footer class='bottom-5 absolute w-full select-none cursor-default'>
-      <p class='text-white text-center font-extrabold'>
-        Please play in desktop mode.
-      </p>
-      <p class='text-white text-center font-extrabold'>
-        Origin game created by ARVI TEIKARI
-      </p>
-      <p class='text-white text-center font-extrabold'>
-        Re-implemented by
-        <a href='https://xcc.tw' rel='noreferrer noopener' target='_blank' text-blue hover:text-lime>TU Xanonymous</a> &
-        <a href='https://ricky-hu.com' rel='noreferrer noopener' target='_blank' text-blue hover:text-lime>RickyHu</a>.
-      </p>
-    </footer>
+
+    <div class='fixed bottom-4 right-4 z-50'>
+      <div
+        v-show='showFooter'
+        class='absolute bottom-14 right-0 w-[min(85vw,360px)] rounded-xl bg-black/80 text-white shadow-xl ring-1 ring-white/15 backdrop-blur px-4 py-3'
+      >
+        <p class='text-center font-extrabold'>
+          Please play in desktop mode.
+        </p>
+        <p class='mt-1 text-center font-extrabold'>
+          Origin game created by ARVI TEIKARI
+        </p>
+        <p class='mt-1 text-center font-extrabold'>
+          Re-implemented by
+          <a href='https://xcc.tw' rel='noreferrer noopener' target='_blank' text-blue hover:text-lime>TU Xanonymous</a> &
+          <a href='https://ricky-hu.com' rel='noreferrer noopener' target='_blank' text-blue hover:text-lime>RickyHu</a>.
+        </p>
+      </div>
+
+      <button
+        class='w-12 h-12 rounded-full bg-black/70 text-white font-extrabold text-xl flex items-center justify-center shadow-lg ring-1 ring-white/20 active:(scale-95)'
+        type='button'
+        aria-label='Info'
+        @click.stop.prevent='toggleFooter'
+      >
+        I
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang='ts' setup>
+  import { ref } from 'vue'
   import babaBackgroundUrl from '@/assets/images/BaBaIsYou.webp'
   import animationBg from '../assets/images/animation-bg.webp'
 
   const animationBgStyleUrlValue = `url('${animationBg}')`
+
+  const showFooter = ref(false)
+
+  const toggleFooter = () => {
+    showFooter.value = !showFooter.value
+  }
 </script>
 
 <style lang='scss' scoped>
